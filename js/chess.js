4449815f619558226f42f5e80c47423faadc4cbb
tomasbalvocius <tomasbalv@gmail.com>
2023-06-22 14:35:54 +0300

    chess update

--- js/chess.js
+++ js/chess.js
@@ -1,10 +1,35 @@
 import { renderHeader } from "../js/header.js";
-
 renderHeader();
 
+const formDOM = document.querySelector("form");
+const inputDOM = formDOM.querySelector("input");
+const boardDOM = document.querySelector(".board");
+
+formDOM.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const number = parseInt(inputDOM.value);
+
+  if (number > 0 && number % 1 === 0) {
+    renderChessBoard(number);
+  }
+});
+
 function renderChessBoard(size = 8) {
-  console.log(size);
+  let blackHTML = "";
+  let whiteHTML = "";
+  let HTML = "";
+
+  for (let m = 0; m < size; m++) {
+    blackHTML += `<div class="cell ${m % 2 === 0 ? "black" : ""}"></div>`;
+    whiteHTML += `<div class="cell ${m % 2 === 0 ? "" : "black"}"></div>`;
+  }
+
+  for (let i = 0; i < size; i++) {
+    HTML += `<div class="row">${i % 2 === 0 ? blackHTML : whiteHTML}</div>`;
+  }
+
+  boardDOM.innerHTML = HTML;
 }
 
-renderChessBoard(4);
 renderChessBoard();
